1898bb242a5ee2144589f84d1425327320460ee9
Mike Manzo <mike.manzo@.com>
2026-04-06 07:28:15 -0400

    Initial Commit

---
+++ EVEOpsTests/EVEOpsTests.swift
@@ -0,0 +1,19 @@
+//
+//  EVEOpsTests.swift
+//  EVEOpsTests
+//
+//  Created by Mike Manzo on 4/6/26.
+//
+
+import Testing
+@testable import EVEOps
+
+struct EVEOpsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
